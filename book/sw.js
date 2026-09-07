ee9b1013d2a8378a8902736de434ba9683ccd181
masinlocandher-max <masinlocandher@gmail.com>
2026-09-07 20:23:20 +0800

    Refresh ebook checkout shell cache

--- book/sw.js
+++ book/sw.js
@@ -8,7 +8,7 @@
   protected chapter returned by the book API. Protected book text stays network
   only and is delivered chapter by chapter after access is verified.
 */
-var VERSION='trwtl-v2';
+var VERSION='trwtl-v3';
 var SHELL=VERSION+'-shell';
 var RUNTIME=VERSION+'-runtime';
 
