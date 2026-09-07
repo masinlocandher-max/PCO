38a358a6b5e1f1da2deae4c4ebee49fade3988d7
masinlocandher-max <masinlocandher@gmail.com>
2026-09-07 21:06:02 +0800

    Point book mobile polish to public root

--- legal-links.js
+++ legal-links.js
@@ -15,7 +15,7 @@
     var link=document.createElement('link');
     link.id='bookMobilePolish';
     link.rel='stylesheet';
-    link.href='/book/mobile-polish.css?v=20260907-1';
+    link.href='/book-mobile-polish.css?v=20260907-2';
     document.head.appendChild(link);
   }
 
